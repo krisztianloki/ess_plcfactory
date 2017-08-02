58cf6ea379622b2ecadd302055b97c10f0cb976b
Krisztián Löki <krisztian.loki@esss.se>
2017-08-02 16:06:45 +0200

    Dynamic PLC db sizing
    Instead of setting the size of the communication data blocks in the PLC
    to 2000 bytes, match their sizes to what is really needed.
    Also, generate the _CommsEPICS function to have the correct size of the
    array to be sent to the IOC

--- template_factory/printers/printer_tia_map.py
+++ template_factory/printers/printer_tia_map.py
@@ -16,7 +16,7 @@ from tf_ifdef import CMD_BLOCK
 
 
 def printer():
-    return [(TIA_MAP.name(), TIA_MAP), (TIA_MAPX.name(), TIA_MAPX)]
+    return [(TIA_MAP.name(), TIA_MAP), (TIA_MAP_NG.name(), TIA_MAP_NG)]
 
 
 
@@ -130,7 +130,7 @@ END_DATA_BLOCK
 
 
 
-class TIA_MAPX(TIA_MAP):
+class TIA_MAP_NG(TIA_MAP):
     def __init__(self, comments = False):
         TIA_MAP.__init__(self, comments)
 
@@ -160,8 +160,8 @@ VERSION : 0.1
 BEGIN
         //Comms data generation hash
         #Hash := DInt##HASH;
-        "[PLCF#PLCToEPICSDataBlockName]"."Word"[1] := DINT_TO_WORD(#Hash);
-        "[PLCF#PLCToEPICSDataBlockName]"."Word"[0] := DINT_TO_WORD(SHR(IN := #Hash, N := 16));
+        "PLCToEPICS"."Word"[1] := DINT_TO_WORD(#Hash);
+        "PLCToEPICS"."Word"[0] := DINT_TO_WORD(SHR(IN := #Hash, N := 16));
 
   // {inst_slot}: PLC <-> EPICS Communication Mapping
   //------------------------------------------------------------------------
@@ -182,8 +182,8 @@ BEGIN
                                           PLCToEPICSDataBlockOffset := [PLCF# ^(PLCToEPICSDataBlockStartOffset) + Counter2],
                                           EPICSToPLCCommandRegisters := "DEV_{inst_slot}_iDB".CommandReg,
                                           PLCToEPICSStatusRegisters := "DEV_{inst_slot}_iDB".StatusReg,
-                                          EPICSToPLCDataBlock := "[PLCF# ^(EPICSToPLCDataBlockName)]"."Word",
-                                          PLCToEPICSDataBlock := "[PLCF# ^(PLCToEPICSDataBlockName)]"."Word");
+                                          EPICSToPLCDataBlock := "EPICSToPLC"."Word",
+                                          PLCToEPICSDataBlock := "PLCToEPICS"."Word");
 #COUNTER Counter1 = [PLCF# Counter1 + {epicstoplclength}];
 #COUNTER Counter2 = [PLCF# Counter2 + {plctoepicslength}];
 """.format(inst_slot          = self.inst_slot(),
@@ -191,3 +191,68 @@ BEGIN
            plctoepicslength   = if_def.from_plc_words_length(),
            commandwordslength = str(if_def.properties()[CMD_BLOCK.length_keyword()])
           ).replace("\n", "\r\n"), output)
+
+
+
+    #
+    # FOOTER
+    #
+    def footer(self, output):
+        PRINTER.footer(self, output)
+
+        self._append("""
+END_FUNCTION
+
+//########## EPICS->PLC datablock ##########
+DATA_BLOCK "EPICSToPLC"
+{ S7_Optimized_Access := 'FALSE' }
+VERSION : 0.1
+NON_RETAIN
+   STRUCT
+      "Word" : Array[0..[PLCF# Counter1 - 1]] of Word;
+   END_STRUCT;
+
+
+BEGIN
+END_DATA_BLOCK
+
+//########## PLC->EPICS datablock ##########
+DATA_BLOCK "PLCToEPICS"
+{ S7_Optimized_Access := 'FALSE' }
+VERSION : 0.1
+NON_RETAIN
+   STRUCT
+      "Word" : Array[0..[PLCF# Counter2 - 1]] of Word;
+   END_STRUCT;
+
+
+BEGIN
+END_DATA_BLOCK
+
+FUNCTION "_CommsEPICS" : Void
+{ S7_Optimized_Access := 'TRUE' }
+VERSION : 0.1
+
+BEGIN
+	//Heartbeat PLC->EPICS
+	IF "Utilities".Pulse_1s THEN
+	    "PLCToEPICS"."Word"[2] := "PLCToEPICS"."Word"[2] + 1;
+	    IF "PLCToEPICS"."Word"[2] >= 32000 THEN
+	        "PLCToEPICS"."Word"[2] := 0;
+	    END_IF;
+	END_IF;
+	
+	// Call the comms block to provide PLC<->EPICS comms
+	"_CommsPLC_EPICS_DB"(Enable:="Utilities".AlwaysOn,
+	                    SendTrigger:="Utilities".Pulse_200ms,
+	                    BytesToSend:=[PLCF# 2 * Counter2],
+	                    InterfaceID:= DINT_TO_WORD(72), // 16#48,
+	                    PLCToEPICSData:="PLCToEPICS"."Word",
+	                    EPICSToPLCData:="EPICSToPLC"."Word");
+	
+	//Map all devices command and status registers to EPICS->PLC and PLC->EPICS data exchange blocks
+	"_CommsEPICSDataMap"();
+	
+END_FUNCTION
+
+""".replace("\n", "\r\n"), output)
