ebe6c9581c8e0d792603a38d01fcba3bc7dfaef5
Krisztián Löki <krisztian.loki@esss.se>
2017-09-07 11:01:26 +0200

    Added PLC Diagnostics printer

---
+++ template_factory/printers/printer_diag.py
@@ -0,0 +1,60 @@
+""" Template Factory: PLC Diagnostics printer """
+
+
+__author__     = "Krisztian Loki"
+__copyright__  = "Copyright 2017, European Spallation Source, Lund"
+__license__    = "GPLv3"
+
+
+
+from printers import PRINTER
+
+
+
+def printer():
+    return (DIAG.name(), DIAG)
+
+
+
+
+class DIAG(PRINTER):
+    def __init__(self, comments = False):
+        PRINTER.__init__(self, comments)
+
+
+    @staticmethod
+    def name():
+        return "DIAG"
+
+
+    #
+    # HEADER
+    #
+    def header(self, output):
+        PRINTER.header(self, output)
+
+        self._append("""#FILENAME {inst_slot}-DiagConfig.txt
+MAX_IO_DEVICES
+{max_io_devices}
+MAX_LOCAL_MODULES
+{max_local_modules}
+MAX_MODULES_IN_IO_DEVICE
+{max_modules_in_io_device}
+""".format(inst_slot                = self.inst_slot(),
+           max_io_devices           = self.plcf("PLC-DIAG:Max-IO-Devices"),
+           max_local_modules        = self.plcf("PLC-DIAG:Max-Local-Modules"),
+           max_modules_in_io_device = self.plcf("PLC-DIAG:Max-Modules-In-IO-Device")), output)
+
+
+    #
+    # BODY
+    #
+    def body(self, if_def, output):
+        PRINTER.body(self, if_def, output)
+
+
+    #
+    # FOOTER
+    #
+    def footer(self, output):
+        PRINTER.footer(self, output)
