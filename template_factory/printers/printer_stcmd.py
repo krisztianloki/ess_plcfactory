7705d6f74197989eab707ef7359dfc584f60adb1
Krisztián Löki <krisztian.loki@esss.se>
2018-02-07 14:18:06 +0100

    Added ST-TEST-CMD and AUTOSAVE-ST-TEST-CMD printers
    Can generate startup scripts for testing with and without autosave
    support

--- template_factory/printers/printer_stcmd.py
+++ template_factory/printers/printer_stcmd.py
@@ -12,14 +12,15 @@ from . import PRINTER
 
 
 def printer():
-    return [ (ST_CMD.name(), ST_CMD), (AUTOSAVE_ST_CMD.name(), AUTOSAVE_ST_CMD) ]
+    return [ (ST_CMD.name(), ST_CMD), (AUTOSAVE_ST_CMD.name(), AUTOSAVE_ST_CMD), (ST_TEST_CMD.name(), ST_TEST_CMD), (AUTOSAVE_ST_TEST_CMD.name(), AUTOSAVE_ST_TEST_CMD) ]
 
 
 
 
 class ST_CMD(PRINTER):
-    def __init__(self):
+    def __init__(self, test = False):
         PRINTER.__init__(self)
+        self._test = test
 
 
     @staticmethod
@@ -33,7 +34,7 @@ class ST_CMD(PRINTER):
     def header(self, output):
         PRINTER.header(self, output)
 
-        st_cmd_header = """#FILENAME {startup}.cmd
+        st_cmd_header = """#FILENAME {startup}{test}.cmd
 # @field PLCNAME
 # @type STRING
 # asyn port name for the PLC
@@ -47,7 +48,8 @@ class ST_CMD(PRINTER):
 # PLC->EPICS receive timeout (ms), should be longer than frequency of PLC SND block trigger (REQ input)
 #COUNTER Counter2 = [PLCF#Counter2 + 10 * 2]
 
-""".format(startup = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())"))
+""".format(startup = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())"),
+           test    = "" if not self._test else "-test")
 
         self._append(st_cmd_header, output)
 
@@ -69,7 +71,8 @@ class ST_CMD(PRINTER):
     def footer(self, output):
         PRINTER.footer(self, output)
 
-        st_cmd_footer = """
+        if not self._test:
+            st_cmd_footer = """
 # Call the EEE module responsible for configuring IOC to PLC comms configuration
 requireSnippet(s7plc-comms.cmd, "PLCNAME=$(PLCNAME), IPADDR=$(IPADDR), S7DRVPORT={s7drvport}, MODBUSDRVPORT={modbusdrvport}, INSIZE={insize}, OUTSIZE=0, BIGENDIAN={bigendian}, RECVTIMEOUT=$(RECVTIMEOUT)")
 
@@ -81,15 +84,32 @@ dbLoadRecords("{modulename}.db", "PLCNAME=$(PLCNAME)")
            bigendian     = self.plcf("1 if 'PLC-EPICS-COMMS:Endianness' == 'BigEndian' else 0"),
            modulename    = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())")
           )
+        else:
+            st_cmd_footer = """
+# Load plc interface database
+dbLoadRecords("{modulename}-test.db", "PLCNAME=$(PLCNAME)")
+""".format(modulename    = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())"))
 
         self._append(st_cmd_footer, output)
 
 
 
 
-class AUTOSAVE_ST_CMD(ST_CMD):
+class ST_TEST_CMD(ST_CMD):
     def __init__(self):
-        PRINTER.__init__(self)
+        ST_CMD.__init__(self, test = True)
+
+
+    @staticmethod
+    def name():
+        return "ST-TEST-CMD"
+
+
+
+
+class AUTOSAVE_ST_CMD(ST_CMD):
+    def __init__(self, **keyword_parameters):
+        ST_CMD.__init__(self, **keyword_parameters)
 
 
     @staticmethod
@@ -129,11 +149,23 @@ set_requestfile_path("$(REQUIRE_{modulename}_PATH)", "misc")
 set_savefile_path("$(SAVEFILE_PATH)", "")
 
 # Specify what save files should be restored
-set_pass0_restoreFile("{modulename}.sav")
+set_pass0_restoreFile("{modulename}{test}.sav")
 
 # Create monitor set
-create_monitor_set("{modulename}.req", 1, "")
-""".format(modulename    = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())")
-          )
+create_monitor_set("{modulename}{test}.req", 1, "")
+""".format(modulename    = self.plcf("ext.to_filename('INSTALLATION_SLOT'.lower())"),
+           test          = "" if not self._test else "-test")
 
         self._append(st_cmd_footer, output)
+
+
+
+
+class AUTOSAVE_ST_TEST_CMD(AUTOSAVE_ST_CMD):
+    def __init__(self):
+        AUTOSAVE_ST_CMD.__init__(self, test = True)
+
+
+    @staticmethod
+    def name():
+        return "AUTOSAVE-ST-TEST-CMD"
