56b262f81d617df5a660517f231de9cb7003bf15
Krisztián Löki <krisztian.loki@esss.se>
2018-07-20 14:46:46 +0200

    Added ARCHIVE printer
    It simply prints the PVs (line-by-line) that need to be archived.
    Just set ARCHIVE=True when defining a variable.

---
+++ template_factory/printers/printer_archive.py
@@ -0,0 +1,57 @@
+""" Template Factory: ARCHIVE printer class """
+
+
+__author__     = "Krisztian Loki"
+__copyright__  = "Copyright 2018, European Spallation Source, Lund"
+__license__    = "GPLv3"
+
+
+#TODO:
+# DOCUMENT, DOCUMENT, DOCUMENT
+
+
+from . import PRINTER
+from tf_ifdef import BASE_TYPE
+
+
+def printer():
+    return (ARCHIVE.name(), ARCHIVE)
+
+
+
+#
+# AA output
+#
+class ARCHIVE(PRINTER):
+    def __init__(self):
+        PRINTER.__init__(self)
+
+
+    @staticmethod
+    def name():
+        return "ARCHIVE"
+
+
+    #
+    # HEADER
+    #
+    def header(self, output, **keyword_params):
+        #
+        # No need to initialize anything
+        #
+        PRINTER.header(self, output, **keyword_params)._append("#FILENAME {inst_slot}-{template}-{timestamp}.txt".format(inst_slot = self.root_inst_slot(),
+                                                                                                                         template  = self.template(),
+                                                                                                                         timestamp = self.timestamp()), output)
+
+
+    #
+    # BODY
+    #
+    def body(self, if_def, output):
+        PRINTER.body(self, if_def, output)
+
+        inst_slot = self.inst_slot()
+        for var in if_def.interfaces():
+            if isinstance(var, BASE_TYPE) and var.get_parameter("ARCHIVE", False):
+                self._append("{inst_slot}:{name}\n".format(inst_slot = inst_slot,
+                                                           name      = var.pv_name()), output)
