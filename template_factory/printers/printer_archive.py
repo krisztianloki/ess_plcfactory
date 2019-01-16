0241f885876ea5df6b9a0f0bf405841f81f9335b
Krisztián Löki <krisztian.loki@esss.se>
2019-01-16 12:01:02 +0100

    .archive syntax has changed
    Now, only a policy can be specified

--- template_factory/printers/printer_archive.py
+++ template_factory/printers/printer_archive.py
@@ -23,9 +23,6 @@ def printer():
 # AA output
 #
 class ARCHIVE(PRINTER):
-    methods = {"SCAN"    : "SCAN",
-               "MONITOR" : "MONITOR"}
-
     def __init__(self):
         super(ARCHIVE, self).__init__()
 
@@ -55,28 +52,32 @@ class ARCHIVE(PRINTER):
     #
     def _ifdef_body(self, if_def, output):
         inst_slot = self.inst_slot()
+        self._append("#" * 60, output)
         for var in if_def.interfaces():
             if isinstance(var, BASE_TYPE) and var.get_parameter("ARCHIVE", False):
-                self._append("{inst_slot}:{name}{spec}\n".format(inst_slot = inst_slot,
-                                                                 name      = var.pv_name(),
-                                                                 spec      = self._archive(var)), output)
+                desc = self._get_desc(var)
+                if desc:
+                    self._append("# {}".format(desc), output)
+                self._append("{inst_slot}:{name}{policy}\n".format(inst_slot = inst_slot,
+                                                                   name      = var.pv_name(),
+                                                                   policy    = self._archive(var)), output)
 
 
     def _archive(self, var):
         arch = var.get_parameter("ARCHIVE")
         if isinstance(arch, bool):
             return ""
-        if isinstance(arch, int) or isinstance(arch, float):
-            return " {}".format(float(arch))
-        elif not isinstance(arch, tuple):
+        if not (isinstance(arch, str) or isinstance(arch, int) or isinstance(arch, float)):
             raise IfDefSyntaxError("Invalid ARCHIVE specification: {} {}".format(arch, type(arch)))
 
-        if len(arch) != 2:
-            raise IfDefSyntaxError("Wrong number of ARCHIVE elements: {}".format(arch))
+        return arch
 
-        try:
-            return " {} {}".format(float(arch[0]), self.methods[arch[1].upper()])
-        except ValueError:
-            raise IfDefSyntaxError("ARCHIVE parameters must start with a number: {}".format(arch))
-        except KeyError:
-            raise IfDefSyntaxError("ARCHIVE second parameter must be either one of {}".format(self.methods.values()))
+
+    def _get_desc(self, var):
+        desc = var.get_parameter("ARCH_DESC", None)
+        if not desc:
+            desc = var.get_parameter("ARCHIVE_DESC", None)
+            if not desc:
+                desc = var.get_parameter("PV_DESC", None)
+
+        return desc
